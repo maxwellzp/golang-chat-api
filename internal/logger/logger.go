d1f12a94e7e515457ceafc8f9166b48666f7b378
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-07-01 15:01:40 +0300

    Encapsulates *zap.SugaredLogger as a private field. Provide Logger methods

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,14 +1,48 @@
 package logger
 
 import (
+	"github.com/maxwellzp/golang-chat-api/internal/config"
 	"go.uber.org/zap"
 )
 
-func NewLogger() (*zap.SugaredLogger, error) {
-	logger, err := zap.NewProduction()
+type Logger struct {
+	log *zap.SugaredLogger
+}
+
+func NewLogger(cfg *config.Config) (*Logger, error) {
+	var zapLogger *zap.Logger
+	var err error
+
+	if cfg.Application.AppEnv == "prod" {
+		zapLogger, err = zap.NewProduction()
+	} else {
+		zapLogger, err = zap.NewDevelopment()
+	}
 	if err != nil {
 		return nil, err
 	}
-	defer logger.Sync()
-	return logger.Sugar(), nil
+	return &Logger{log: zapLogger.Sugar()}, nil
+}
+
+func (l *Logger) Fatalw(msg string, keysAndValues ...any) {
+	l.log.Fatalw(msg, keysAndValues...)
+}
+
+func (l *Logger) Infow(msg string, keysAndValues ...any) {
+	l.log.Infow(msg, keysAndValues...)
+}
+
+func (l *Logger) Warnw(msg string, keysAndValues ...any) {
+	l.log.Warnw(msg, keysAndValues...)
+}
+
+func (l *Logger) Errorw(msg string, keysAndValues ...any) {
+	l.log.Errorw(msg, keysAndValues...)
+}
+
+func (l *Logger) Debugw(msg string, keysAndValues ...any) {
+	l.log.Debugw(msg, keysAndValues...)
+}
+func (l *Logger) Panicw(msg string, keysAndValues ...any) {
+	l.log.Panicw(msg, keysAndValues...)
 }
