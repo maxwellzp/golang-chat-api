4b19fd0e47ad9229f7ee851d35b0a7da0cf98e77
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-07-01 11:24:53 +0300

    Create instances of repositories and services. Adjust constructor method for HTTP handlers.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"github.com/maxwellzp/golang-chat-api/internal/auth"
 	"github.com/maxwellzp/golang-chat-api/internal/config"
+	"github.com/maxwellzp/golang-chat-api/internal/db"
 	"github.com/maxwellzp/golang-chat-api/internal/logger"
 	"github.com/maxwellzp/golang-chat-api/internal/message"
 	"github.com/maxwellzp/golang-chat-api/internal/room"
@@ -19,11 +21,26 @@ func main() {
 	cfg := config.Load(sugar)
 	sugar.Infof("%+v", cfg)
 
+	dbInstance, err := db.NewDb(context.Background(), cfg)
+	if err != nil {
+		log.Fatalf("failed to initialize db: %v", err)
+	}
+
+	// Database Repositories
+	authRepo := auth.NewAuthRepository(dbInstance)
+	roomRepo := room.NewRoomRepository(dbInstance)
+	messageRepo := message.NewMessageRepository(dbInstance)
+
+	// Business logic services
+	authService := auth.NewAuthService(authRepo)
+	roomService := room.NewRoomService(roomRepo)
+	messageService := message.NewMessageService(messageRepo)
+
 	// REST API Handlers
 	router := http.NewServeMux()
-	auth.NewAuthHandler(router)
-	room.NewRoomHandler(router)
-	message.NewMessageHandler(router)
+	auth.NewAuthHandler(router, authService)
+	room.NewRoomHandler(router, roomService)
+	message.NewMessageHandler(router, messageService)
 
 	server := &http.Server{
 		Addr:    ":" + cfg.Server.Port,
