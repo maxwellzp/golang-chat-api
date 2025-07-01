8d850698053a04f4cb15a19b3012b08d896fb88a
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-07-01 14:59:31 +0300

    Improve loading env variables process. Add additional validation

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+type ApplicationConfig struct {
+	AppEnv string
+}
+
 type DbConfig struct {
 	PostgresUser     string
 	PostgresPassword string
@@ -19,25 +23,53 @@ type ServerConfig struct {
 }
 
 type Config struct {
-	Db     DbConfig
-	Server ServerConfig
+	Application ApplicationConfig
+	Db          DbConfig
+	Server      ServerConfig
 }
 
 func Load(logger *zap.SugaredLogger) *Config {
 	// Load environment variables
 	if err := godotenv.Load(); err != nil {
+		// Environment variables can also be set via terminal, bash script ...
+		// APP_ENV=dev go run cmd/api/main.go
 		logger.Warnw("No .env file found")
 	}
+
 	return &Config{
+		Application: ApplicationConfig{
+			AppEnv: getEnv(logger, "APP_ENV", "prod"),
+		},
 		Db: DbConfig{
-			PostgresUser:     os.Getenv("POSTGRES_USER"),
-			PostgresPassword: os.Getenv("POSTGRES_PASSWORD"),
-			PostgresDatabase: os.Getenv("POSTGRES_DB"),
-			PostgresHost:     os.Getenv("POSTGRES_HOST"),
-			PostgresPort:     os.Getenv("POSTGRES_PORT"),
+			PostgresUser:     mustGetEnv(logger, "POSTGRES_USER"),
+			PostgresPassword: mustGetEnv(logger, "POSTGRES_PASSWORD"),
+			PostgresDatabase: mustGetEnv(logger, "POSTGRES_DB"),
+			PostgresHost:     mustGetEnv(logger, "POSTGRES_HOST"),
+			PostgresPort:     getEnv(logger, "POSTGRES_PORT", "5432"),
 		},
 		Server: ServerConfig{
-			Port: os.Getenv("SERVER_PORT"),
+			Port: getEnv(logger, "SERVER_PORT", "8080"),
 		},
 	}
+}
+
+func getEnv(logger *zap.SugaredLogger, key, defaultVal string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	logger.Infow("Using default value for env variable",
+		"environment variable", key,
+		"default", defaultVal,
+	)
+	return defaultVal
+}
+
+func mustGetEnv(logger *zap.SugaredLogger, key string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok || value == "" {
+		logger.Fatalw("Required environment variable is not set",
+			"key", key,
+		)
+	}
+	return value
 }
