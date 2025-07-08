c6d17abd377a879dd660aa87bf9a730358d45a6b
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-07-08 14:03:34 +0300

    Register custom validations

--- internal/validatorx/validator.go
+++ internal/validatorx/validator.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-playground/validator/v10"
 	"reflect"
 	"strings"
+	"unicode"
 )
 
 type Validator struct {
@@ -22,9 +23,54 @@ func NewValidator() *Validator {
 		return name
 	})
 
+	// Register custom validations
+	v.RegisterValidation("containsuppercase", containsUpperCase)
+	v.RegisterValidation("containslowercase", containsLowerCase)
+	v.RegisterValidation("containsnumber", containsNumber)
+	v.RegisterValidation("containsspecial", containsSpecial)
+
 	return &Validator{validate: v}
 }
 
 func (v *Validator) Validate(i interface{}) error {
 	return v.validate.Struct(i)
+}
+
+func containsUpperCase(fl validator.FieldLevel) bool {
+	for _, char := range fl.Field().String() {
+		if unicode.IsUpper(char) {
+			return true
+		}
+	}
+	return false
+}
+
+func containsLowerCase(fl validator.FieldLevel) bool {
+	for _, char := range fl.Field().String() {
+		if unicode.IsLower(char) {
+			return true
+		}
+	}
+	return false
+}
+
+func containsNumber(fl validator.FieldLevel) bool {
+	for _, char := range fl.Field().String() {
+		if unicode.IsNumber(char) {
+			return true
+		}
+	}
+	return false
+}
+
+func containsSpecial(fl validator.FieldLevel) bool {
+	specialChars := "!@#$%^&*"
+	fieldValue := fl.Field().String()
+
+	for _, char := range specialChars {
+		if strings.ContainsRune(fieldValue, char) {
+			return true
+		}
+	}
+	return false
 }
