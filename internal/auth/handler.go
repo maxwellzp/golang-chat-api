2a0778bd4b673a011f6c71a1a7a64a754a2a3221
Maksim Evtyagin <evtyagin.maksim@gmail.com>
2025-07-09 13:56:27 +0300

    Add logs to AuthHandler

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"encoding/json"
 	"github.com/maxwellzp/golang-chat-api/internal/httpx"
+	"github.com/maxwellzp/golang-chat-api/internal/logger"
 	"github.com/maxwellzp/golang-chat-api/internal/validatorx"
 	"net/http"
 )
@@ -10,12 +11,14 @@ import (
 type AuthHandler struct {
 	authService *AuthService
 	validator   *validatorx.Validator
+	logger      *logger.Logger
 }
 
-func NewAuthHandler(authService *AuthService, validator *validatorx.Validator) *AuthHandler {
+func NewAuthHandler(authService *AuthService, validator *validatorx.Validator, logger *logger.Logger) *AuthHandler {
 	return &AuthHandler{
 		authService: authService,
 		validator:   validator,
+		logger:      logger,
 	}
 }
 
@@ -23,20 +26,35 @@ func (h *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req LoginRequest
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			h.logger.Warnw("Login request JSON decode failed",
+				"error", err,
+			)
 			httpx.WriteError(w, http.StatusBadRequest, "Invalid request body")
 			return
 		}
 
 		if err := h.validator.Validate(&req); err != nil {
+			h.logger.Infow("Login request validation failed",
+				"email", req.Email,
+				"errors", err,
+			)
 			httpx.WriteValidationError(w, err)
 			return
 		}
 
-		_, token, err := h.authService.Login(r.Context(), req.Email, req.Password)
+		user, token, err := h.authService.Login(r.Context(), req.Email, req.Password)
 		if err != nil {
+			h.logger.Warnw("Login failed",
+				"email", req.Email,
+				"error", err,
+			)
 			httpx.WriteError(w, http.StatusUnauthorized, err.Error())
 			return
 		}
+		h.logger.Infow("User logged in successfully",
+			"user_id", user.ID,
+			"email", req.Email,
+		)
 		resp := LoginResponse{
 			Token: token,
 		}
@@ -47,20 +65,35 @@ func (h *AuthHandler) Register() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req RegisterRequest
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			h.logger.Warnw("Register request JSON decode failed",
+				"error", err,
+			)
 			httpx.WriteError(w, http.StatusBadRequest, "Invalid request body")
 			return
 		}
 
 		if err := h.validator.Validate(&req); err != nil {
+			h.logger.Infow("Register request validation failed",
+				"email", req.Email,
+				"errors", err,
+			)
 			httpx.WriteValidationError(w, err)
 			return
 		}
 
-		u, err := h.authService.Register(r.Context(), req.Username, req.Email, req.Password)
+		user, err := h.authService.Register(r.Context(), req.Username, req.Email, req.Password)
 		if err != nil {
+			h.logger.Warnw("User registration failed",
+				"email", req.Email,
+				"error", err,
+			)
 			httpx.WriteError(w, http.StatusBadRequest, err.Error())
 			return
 		}
-		httpx.WriteJSON(w, http.StatusCreated, u)
+		h.logger.Infow("User registered successfully",
+			"user_id", user.ID,
+			"email", req.Email,
+		)
+		httpx.WriteJSON(w, http.StatusCreated, user)
 	}
 }
